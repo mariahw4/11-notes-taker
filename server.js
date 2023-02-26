ace6af0214c206c592a4e76add8467f2bd444813
Mariah Wear <wearmariahk@gmail.com>
2023-02-26 13:33:29 -0700

    adding gitignore and server.js file

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
+const express = require("express");
+const db = require("./db/db.json");
+var path = require("path");
 
+const app = express();
 
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Express will serve static files in public and data directories
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("App listening on http://localhost: " + PORT);
+});
 
 // // The application should have a db.json file on the back end that will be used to store and retrieve notes using the fs module.
 
